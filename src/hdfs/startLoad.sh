e0d27df9750e110378cfc9e6d911a46730db02e5
aaarl <arlrobin.ferhati@gmail.com>
2020-12-19 13:52:15 +0100

    added script for start load in hdfs and fixed remining errors

---
+++ src/hdfs/startLoad.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+echo "Start loading Data in HDFS"
+
+echo "Create input folder and load data"
+kubectl exec -ti my-hadoop-cluster-hadoop-yarn-rm-0 -- bash -c "hdfs dfs -mkdir -p input && hdfs dfs -chmod -R 777 input && curl https://opendata.ecdc.europa.eu/covid19/casedistribution/csv/ | hdfs dfs -put - input/spotifydata"
+
+echo "List folder 'input'"
+kubectl exec -ti my-hadoop-cluster-hadoop-yarn-rm-0 -- bash -c "hdfs dfs -ls input"
+
+echo "Loading data completed"
+
+echo "Get IP of the load balancer"
+KNOX_IP=`kubectl get svc knox-apache-knox-helm-svc \
+        '-o=jsonpath={.status.loadBalancer.ingress[0].ip}'`
+echo $KNOX_IP
+
+kubectl describe service/knox-apache-knox-helm-svc
+echo "1) Check application at KNOX Endpoint IP: "
+echo "> http://KNOX_IP:8080/yarn/"
+echo "> http://KNOX_IP:8080/hdfs/"
+echo "2) Use the following login credentials: "
+echo "> User: admin"
+echo "> PW: admin-password"
+echo "3) Access Data Lake via WebHDFS REST API"
+echo "> List a driectory: curl -v -u admin:admin-password 'http://KNOX_IP:8080/webhdfs/v1/user/root/input/?op=LISTSTATUS'"
+echo "> Read and open file: curl -v -u admin:admin-password -i -L 'http://KNOX_IP:8080/webhdfs/v1/user/root/input/spotifydata?op=OPEN'"
